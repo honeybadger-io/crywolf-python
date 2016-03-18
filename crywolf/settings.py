ed21ba2b5c7b87b0e71f7b35e3c6d2d28788a9ac
Joshua Wood <josh@hintmedia.com>
2016-03-18 10:11:18 -0700

    Add Django logging config.

--- crywolf/settings.py
+++ crywolf/settings.py
@@ -120,3 +120,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+    'console': {
+        'level': 'DEBUG',
+        'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+    'honeybadger': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': True,
+        },
+    },
+}
