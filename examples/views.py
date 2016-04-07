81338f0a064426613bcc1d4088a81513d4e590e6
joshuap <josh@joshuawood.net>
2016-04-07 11:16:37 -0700

    Update views.py

--- examples/views.py
+++ examples/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from honeybadger import honeybadger
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the crywolf index.<br><br>Looking to <a href='/fail/?foo=bar&amp;bar=baz&amp;user_email=josh@honeybadger.io'>fail</a>?")
+    return HttpResponse("Hello, world. You're at the crywolf index.<br><br>Looking to <a href='/fail/?foo=bar&amp;bar=baz&amp;user_email=user@example.com'>fail</a>?")
 
 def fail(request):
     with honeybadger.context(user_email=request.GET.get('user_email', None)):
